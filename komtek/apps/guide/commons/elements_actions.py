cf6b148f055057e2b75e11da471c891f99dc75a2
m0r0zz1rk <97999819+m0r0zz1rk@users.noreply.github.com>
2023-08-09 16:32:53 +0800

    Update elements_actions.py

--- komtek/apps/guide/commons/elements_actions.py
+++ komtek/apps/guide/commons/elements_actions.py
@@ -4,8 +4,6 @@ class ElementsActions:
     @staticmethod
     def check_element_exist(qs, code, value) -> bool:
         """Проверка существующего элемента по коду и значению в полученном queryset"""
-        print(code)
-        print(value)
         try:
             return qs.filter(code=code).filter(value=value).exists()
         except Exception:
